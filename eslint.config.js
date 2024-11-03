da949c73b03fd050087212cf430fba309bb194e9
adhilansari <frkadhilansaripv@gmail.com.com>
2024-11-03 23:33:00 +0530

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      "@angular-eslint/prefer-standalone": ["error"], //standalone architecture
+      // "@angular-eslint/prefer-on-push-component-change-detection": ["error"], // zoneless change detection
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {
+      "@angular-eslint/template/prefer-self-closing-tags": ["error"], //self-closing components tags
+      "@angular-eslint/template/prefer-control-flow": ["error"], //built-in control flow syntax
+      "@angular-eslint/template/prefer-ngsrc": ["error"], //optimized images
+
+    },
+  }
+);
